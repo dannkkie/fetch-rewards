00a1e59bc36a6c4a9baaa426683177307fa4c167
Eddie <ekkpodogui@gmail.com>
2020-10-11 16:14:55 +0000

    Add navigation bar

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,19 @@
   <v-app>
     <v-app-bar
       app
-      color="primary"
+      color="white"
       dark
     >
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+
 
         <v-img
           alt="Vuetify Name"
           class="shrink mt-1 hidden-sm-and-down"
           contain
           min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
+          src="@/assets/logo.png"
+          width="150"
         />
       </div>
 
@@ -30,9 +23,10 @@
       <v-btn
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
         target="_blank"
+        color="black"
         text
       >
-        <span class="mr-2">Latest Release</span>
+        <span class="mr-2">Main Page</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
