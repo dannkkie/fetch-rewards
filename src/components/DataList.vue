48f646cc18cc3341bdafa783c2e85785f8fbfb7b
Eddie <ekkpodogui@gmail.com>
2020-10-12 01:31:08 +0000

    Add filtering, sorting and grouping feature

--- src/components/DataList.vue
+++ src/components/DataList.vue
@@ -2,9 +2,10 @@
   <v-container>
     <v-data-table
     :headers="headers"
-    :items="item"
-    :items-per-page="10"
-    class="elevation-1"
+    :items="results"
+    :items-per-page="50"
+    group-by="listId"
+    class="elevation-8 mt-12"
     >
     </v-data-table>
   </v-container>
@@ -26,7 +27,34 @@
           value: 'name',
         },
       ],
-      results: []
+
+      results: [],
     }),
+
+    created() {
+      this.fetchData();
+    },
+
+    methods: {
+      async fetchData() {
+        let proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+        let targetUrl = 'https://fetch-hiring.s3.amazonaws.com/hiring.json';
+        try{
+          const response = await fetch(proxyUrl + targetUrl);
+          const results = await response.json();
+          const filteredResults = results.filter(result => result.name !== null && result.name !== '');
+          const sortedResultsByName = filteredResults.sort((first, second) => first['name'] > second['name']);
+          const sortedResultsByListId = sortedResultsByName.sort((first, second) => first['listId'] - second['listId']);
+          
+        //   const sortedResultsByListId = filteredResults.sort((first, second) => first['listId'] - second['listId']);
+        //   const sortedResultsByName = sortedResultsByListId.sort((first, second) => first['name'] > second['name']);
+
+          this.results = sortedResultsByListId;
+          console.log(this.results)
+          } catch(error) {
+            console.log(error);
+          }
+      }
+    }
   }
 </script>
